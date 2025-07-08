d2f6aa9bad978ed63c447efa927f3634215382b1
Bookbells <isabelle.berg90@gmail.com>
2025-07-08 14:58:58 +0200

    Update example-keys.py

--- code/example-keys.py
+++ code/example-keys.py
@@ -12,6 +12,8 @@ MQTT_USER = "{your_user_name}" #change
 MQTT_KEY = "{your_password}" #change
 
 # topics
-MQTT_TOPIC_HUMIDITY = "humidity"
-MQTT_TOPIC_TEMP = "temperature"
-MQTT_TOPIC_LIGHT = "brightness"
+MQTT_AMBIENT_HUMIDITY = "humidity/devices"
+MQTT_BUCKET_HUMIDITY = "root-humidity/devices"
+MQTT_AMBIENT_TEMP = "temperature/devices"
+MQTT_BUCKET_TEMP ="root-temperature/devices"
+MQTT_TOPIC_LIGHT = "brightness/devices"
